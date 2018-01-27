f38f295acb2df02bc26ed3043d73e7030ee2b89f
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2018-01-27 15:55:57 +0100

    Simplification via local macros in Code/Reader/fixup.lisp.

--- Code/Reader/fixup.lisp
+++ Code/Reader/fixup.lisp
@@ -11,40 +11,31 @@
   (declare (ignore seen-objects mapping))
   nil)
 
-(defmethod fixup ((object cons) seen-objects mapping)
-  (multiple-value-bind (value found-p)
-      (gethash (car object) mapping)
-    (if found-p
-        (setf (car object) value)
-        (fixup (car object) seen-objects mapping)))
-  (multiple-value-bind (value found-p)
-      (gethash (cdr object) mapping)
-    (if found-p
-        (setf (cdr object) value)
-        (fixup (cdr object) seen-objects mapping))))
+(macrolet ((fixup-place (place)
+             `(multiple-value-bind (value found-p)
+                  (gethash ,place mapping)
+                (if found-p
+                    (setf ,place value)
+                    (fixup ,place seen-objects mapping)))))
 
-(defmethod fixup ((object array) seen-objects mapping)
-  (loop for i from 0 below (array-total-size object)
-        do (multiple-value-bind (value found-p)
-               (gethash (row-major-aref object i) mapping)
-             (if found-p
-                 (setf (row-major-aref object i) value)
-                 (fixup (row-major-aref object i) seen-objects mapping)))))
+  (defmethod fixup ((object cons) seen-objects mapping)
+    (fixup-place (car object))
+    (fixup-place (cdr object)))
 
-(defmethod fixup ((object standard-object) seen-objects mapping)
-  (loop for slot-definition in (closer-mop:class-slots (class-of object))
-        for name = (closer-mop:slot-definition-name slot-definition)
-        do (multiple-value-bind (value found-p)
-               (gethash (slot-value object name) mapping)
-             (if found-p
-                 (setf (slot-value object name) value)
-                 (fixup (slot-value object name) seen-objects mapping)))))
+  (defmethod fixup ((object array) seen-objects mapping)
+    (loop for i from 0 below (array-total-size object)
+       do (fixup-place (row-major-aref object i))))
 
-(defmethod fixup ((object hash-table) seen-objects mapping)
-  (maphash (lambda (key val)
-             (multiple-value-bind (value found-p)
-                 (gethash val mapping)
-               (if found-p
-                   (setf (gethash key object) value)
-                   (fixup value seen-objects mapping))))
-           object))
+  (defmethod fixup ((object standard-object) seen-objects mapping)
+    (loop for slot-definition in (closer-mop:class-slots (class-of object))
+       for name = (closer-mop:slot-definition-name slot-definition)
+       do (fixup-place (slot-value object name))))
+
+  (defmethod fixup ((object hash-table) seen-objects mapping)
+    (maphash (lambda (key val)
+               (multiple-value-bind (value found-p)
+                   (gethash val mapping)
+                 (if found-p
+                     (setf (gethash key object) value)
+                     (fixup value seen-objects mapping))))
+             object)))
