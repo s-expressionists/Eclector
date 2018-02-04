b393f3341ae89f0da647f5d3d8d773ee25e2b774
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2018-02-04 16:42:39 +0100

    A few non-default *READ-BASE* test cases for INTERPRET-TOKEN.

--- Test/Reader/tokens.lisp
+++ Test/Reader/tokens.lisp
@@ -6,9 +6,10 @@
 (test interpret-token.default.smoke
   "Smoke test for default method on INTERPRET-TOKEN."
 
-  (map nil (lambda (arguments-expected)
-             (destructuring-bind (token token-escapes expected)
-                 arguments-expected
+  (map nil (lambda (arguments-context-expected)
+             (destructuring-bind
+                   (token token-escapes *read-base* expected)
+                 arguments-context-expected
                (let ((token-escapes (if token-escapes
                                         (map 'vector #'plusp token-escapes)
                                         (make-array (length token)
@@ -35,56 +36,82 @@
                         (do-it)))
                      (t
                       (is (equal expected (do-it)))))))))
-       ;; TODO *read-case*, *read-base* parameters
        '(;; empty
-         (""           nil   ||)
+         (""           nil   10 ||)
          ;; "consing dot"
-         ("."          nil   eclector.reader:invalid-context-for-consing-dot)
+         ("."          nil   10 eclector.reader:invalid-context-for-consing-dot)
 
          ;; symbol
-         ("a"          #*1   |a|)
-         ("-"          nil   -)
-         ("+"          nil   +)
+         ("a"          #*1   10 |a|)
+         ("-"          nil   10 -)
+         ("+"          nil   10 +)
 
-         ("-a"         #*01  |-a|)
-         ("-."         nil   -.)
-         ("-:"         nil   eclector.reader:symbol-name-must-not-end-with-package-marker)
-         ("-a"         nil   -a)
+         ("-a"         #*01  10 |-a|)
+         ("-."         nil   10 -.)
+         ("-:"         nil   10 eclector.reader:symbol-name-must-not-end-with-package-marker)
+         ("-a"         nil   10 -a)
 
-         (".a"         #*01  |.a|)
-         (".:"         nil   eclector.reader:symbol-name-must-not-end-with-package-marker)
-         (".a"         nil   .a)
+         (".a"         #*01  10 |.a|)
+         (".:"         nil   10 eclector.reader:symbol-name-must-not-end-with-package-marker)
+         (".a"         nil   10 .a)
 
-         ("-.a"        #*001 |-.a|)
-         ("-.:"        nil   eclector.reader:symbol-name-must-not-end-with-package-marker)
-         ("-.a"        nil   -.a)
+         ("-.a"        #*001 10 |-.a|)
+         ("-.:"        nil   10 eclector.reader:symbol-name-must-not-end-with-package-marker)
+         ("-.a"        nil   10 -.a)
 
-         ("aa"         #*01  |Aa|)
-         ("a:"         nil   eclector.reader:symbol-name-must-not-end-with-package-marker)
-         ("a::"        nil   eclector.reader:symbol-name-must-not-end-with-package-marker)
-         ("a:::"       nil   eclector.reader:symbol-can-have-at-most-two-package-markers)
-         ("a:a:"       nil   eclector.reader:two-package-markers-must-be-adjacent)
-         ("::a"        nil   eclector.reader:two-package-markers-must-not-be-first)
-         ("keyword:b"  nil   :b)
-         ("keyword::b" nil   :b)
+         ("1a"         nil   10 1a)
+         ("1a"         #*01  10 |1a|)
+         ("1:"         nil   10 eclector.reader:symbol-name-must-not-end-with-package-marker)
+         ("1.a"        #*001 10 |1.a|)
+         ("1.:"        nil   10 eclector.reader:symbol-name-must-not-end-with-package-marker)
+         ("1.a"        nil   10 1.a)
+
+         ("1aa"        #*001 16 |1Aa|)
+         ("1a/"        nil   16 1a/)
+         ("1a:"        nil   16 eclector.reader:symbol-name-must-not-end-with-package-marker)
+         ("1ax"        nil   16 1ax)
+
+         ("aa"         #*01  10 |Aa|)
+         ("a:"         nil   10 eclector.reader:symbol-name-must-not-end-with-package-marker)
+         ("a::"        nil   10 eclector.reader:symbol-name-must-not-end-with-package-marker)
+         ("a:::"       nil   10 eclector.reader:symbol-can-have-at-most-two-package-markers)
+         ("a:a:"       nil   10 eclector.reader:two-package-markers-must-be-adjacent)
+         ("::a"        nil   10 eclector.reader:two-package-markers-must-not-be-first)
+         ("keyword:b"  nil   10 :b)
+         ("keyword::b" nil   10 :b)
+
+         ;; decimal-integer
+         ("1"          nil   10    1)
+         ("-1"         nil   10   -1)
+         ("-12"        nil   10  -12)
+
+         ("1."         nil   10    1)
 
          ;; ratio
-         ("+1/2"       nil      1/2)
-         ("-1/2"       nil     -1/2)
-         ("1/2"        nil      1/2)
+         ("+1/2"       nil   10    1/2)
+         ("-1/2"       nil   10   -1/2)
+         ("1/2"        nil   10    1/2)
 
          ;; float-no-exponent
-         ("+.234"      nil       .234)
-         ("-.234"      nil      -.234)
-         (".234"       nil       .234)
-         ("+1.234"     nil      1.234)
-         ("-1.234"     nil     -1.234)
-         ("1.234"      nil      1.234)
+         ("+.234"      nil   10     .234)
+         ("-.234"      nil   10    -.234)
+         (".234"       nil   10     .234)
+         ("+1.234"     nil   10    1.234)
+         ("-1.234"     nil   10   -1.234)
+         ("1.234"      nil   10    1.234)
 
          ;; float-exponent
-         ("+1.0e2"     nil    100.0)
-         ("+1.0e-2"    nil      0.01)
-         ("-1.0e2"     nil   -100.0)
-         ("-1.0e-2"    nil     -0.01)
-         ("1.0e2"      nil    100.0)
-         ("1.0e-2"     nil      0.01))))
+         ("+1.0e2"     nil   10  100.0)
+         ("+1.0e-2"    nil   10    0.01)
+         ("-1.0e2"     nil   10 -100.0)
+         ("-1.0e-2"    nil   10   -0.01)
+         ("1.0e2"      nil   10  100.0)
+         ("1.0e-2"     nil   10    0.01)
+         ("1.e2"       nil   10  100.0)
+         ("1e2"        nil   10  100.0)
+
+         ;; Nondefault *READ-BASE*
+         ("a"          nil   16   10)
+         ("-a"         nil   16  -10)
+         ("1a"         nil   16   26)
+         ("1aa"        nil   16  426))))
