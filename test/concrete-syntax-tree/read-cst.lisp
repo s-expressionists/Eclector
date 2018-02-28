38b988d3c380907ae81712bf59de215039ab7dda
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2018-02-28 12:30:35 +0100

    concrete-syntax-tree: Slightly better smoke test

--- test/concrete-syntax-tree/read-cst.lisp
+++ test/concrete-syntax-tree/read-cst.lisp
@@ -5,5 +5,16 @@
 (test read-cst/smoke
   "Smoke test for the READ-CST function."
 
-  (finishes (with-input-from-string (stream "(cons 1 2)")
-              (eclector.concrete-syntax-tree:cst-read stream))))
+  (map nil (lambda (input-and-expected)
+             (destructuring-bind (input expected) input-and-expected
+               (flet ((do-it ()
+                        (with-input-from-string (stream input)
+                          (values (eclector.concrete-syntax-tree:cst-read stream)
+                                  (file-position stream)))))
+                 (case expected
+                   (t
+                    (multiple-value-bind (result position) (do-it)
+                      (is (typep result 'cst:cst))
+                      (is (eql (length input) position))))))))
+
+       '(("(cons 1 2)" t))))
