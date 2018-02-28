6ed8c074f1e69fbeda9ab41558bf7e0e2ee41c70
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2018-02-28 13:36:23 +0100

    tests: Consistently use MAPC for mapping over test cases

--- test/concrete-syntax-tree/read-cst.lisp
+++ test/concrete-syntax-tree/read-cst.lisp
@@ -5,17 +5,17 @@
 (test read-cst/smoke
   "Smoke test for the READ-CST function."
 
-  (map nil (lambda (input-and-expected)
-             (destructuring-bind (input expected) input-and-expected
-               (flet ((do-it ()
-                        (with-input-from-string (stream input)
-                          (values (eclector.concrete-syntax-tree:cst-read stream)
-                                  (file-position stream)))))
-                 (case expected
-                   (t
-                    (multiple-value-bind (result position) (do-it)
-                      (is (typep result 'cst:cst))
-                      (is (eql (length input) position))))))))
+  (mapc (lambda (input-and-expected)
+          (destructuring-bind (input expected) input-and-expected
+            (flet ((do-it ()
+                     (with-input-from-string (stream input)
+                       (values (eclector.concrete-syntax-tree:cst-read stream)
+                               (file-position stream)))))
+              (case expected
+                (t
+                 (multiple-value-bind (result position) (do-it)
+                   (is (typep result 'cst:cst))
+                   (is (eql (length input) position))))))))
 
-       '(("(cons 1 2)"  t)
-         ("#+(or) `1 2" t))))
+        '(("(cons 1 2)"  t)
+          ("#+(or) `1 2" t))))
--- test/reader/read.lisp
+++ test/reader/read.lisp
@@ -8,17 +8,17 @@
   ;; This test focuses on interactions between different parts of the
   ;; reader since the individual parts in isolation are handled by
   ;; more specific tests.
-  (map nil (lambda (input-and-expected)
-             (destructuring-bind (input expected) input-and-expected
-               (flet ((do-it ()
-                        (with-input-from-string (stream input)
-                          (values (eclector.reader:read stream)
-                                  (file-position stream)))))
-                 (case expected
-                   (t
-                    (multiple-value-bind (result position) (do-it)
-                      (is (equal expected       result))
-                      (is (eql   (length input) position))))))))
+  (mapc (lambda (input-and-expected)
+          (destructuring-bind (input expected) input-and-expected
+            (flet ((do-it ()
+                     (with-input-from-string (stream input)
+                       (values (eclector.reader:read stream)
+                               (file-position stream)))))
+              (case expected
+                (t
+                 (multiple-value-bind (result position) (do-it)
+                   (is (equal expected       result))
+                   (is (eql   (length input) position))))))))
 
-       '(("(cons 1 2)"  (cons 1 2))
-         ("#+(or) `1 2" 2))))
+        '(("(cons 1 2)"  (cons 1 2))
+          ("#+(or) `1 2" 2))))
