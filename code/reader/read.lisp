47b031f10a3fd452b042672d372299fe89c68113
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2018-03-06 12:19:45 +0100

    Simplification in READ-AUX
    *PRESERVE-WHITESPACE* is bound in both branches.

--- code/reader/read.lisp
+++ code/reader/read.lisp
@@ -4,23 +4,22 @@
 
 (defun read-aux
     (input-stream eof-error-p eof-value recursive-p preserve-whitespace-p)
-  (if recursive-p
-      (let ((*preserve-whitespace* preserve-whitespace-p))
-        (read-common *client* input-stream eof-error-p eof-value))
-      (let* ((*preserve-whitespace* preserve-whitespace-p)
-             (*labels* (make-hash-table))
-             (result (read-common *client* input-stream eof-error-p eof-value)))
-        ;; *labels* maps labels to conses of the form
-        ;; (TEMPORARY-OBJECT . FINAL-OBJECT). For the fixup step,
-        ;; these conses into a hash-table mapping temporary objects to
-        ;; final objects.
-        (unless (zerop (hash-table-count *labels*))
-          (let ((seen (make-hash-table :test #'eq))
-                (mapping (alexandria:alist-hash-table
-                          (alexandria:hash-table-values *labels*)
-                          :test #'eq)))
-            (fixup result seen mapping)))
-        result)))
+  (let ((*preserve-whitespace* preserve-whitespace-p))
+    (if recursive-p
+        (read-common *client* input-stream eof-error-p eof-value)
+        (let* ((*labels* (make-hash-table))
+               (result (read-common *client* input-stream eof-error-p eof-value)))
+          ;; *labels* maps labels to conses of the form
+          ;; (TEMPORARY-OBJECT . FINAL-OBJECT). For the fixup step,
+          ;; these conses into a hash-table mapping temporary objects to
+          ;; final objects.
+          (unless (zerop (hash-table-count *labels*))
+            (let ((seen (make-hash-table :test #'eq))
+                  (mapping (alexandria:alist-hash-table
+                            (alexandria:hash-table-values *labels*)
+                            :test #'eq)))
+              (fixup result seen mapping)))
+          result))))
 
 (defun read (&optional
                (input-stream *standard-input*)
