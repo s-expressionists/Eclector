3e46020171503767291e6467ba048e3e528aa306
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2018-04-28 05:11:32 +0200

    Better support for explicit stream position in %READER-ERROR

--- code/base/conditions.lisp
+++ code/base/conditions.lisp
@@ -1,11 +1,12 @@
 (cl:in-package #:eclector.base)
 
-(defun %reader-error (stream datum &rest arguments)
-  (let ((position (ignore-errors (file-position stream))))
-    (apply #'error datum (append arguments
-                                 (list :stream stream)
-                                 (when position
-                                   (list :stream-position position))))))
+(defun %reader-error (stream datum
+                      &rest arguments
+                      &key
+                      (stream-position (ignore-errors (file-position stream)))
+                      &allow-other-keys)
+  (apply #'error datum :stream stream :stream-position stream-position
+         (alexandria:remove-from-plist arguments :stream-position)))
 
 (define-condition stream-position-reader-error (acclimation:condition reader-error)
   ((%stream-position :initarg :stream-position
