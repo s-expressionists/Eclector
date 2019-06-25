7dbdce5240fcb46f5a914c1dcbc260c08aa8f868
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2019-06-25 18:02:50 +0200

    ideas

--- test/parse-result/package.lisp
+++ test/parse-result/package.lisp
@@ -1,6 +1,7 @@
 (cl:defpackage #:eclector.parse-result.test
   (:use
    #:common-lisp
+   #:alexandria
    #:fiveam)
 
   (:import-from #:eclector.test
@@ -16,3 +17,22 @@
 
 (defun run-tests ()
   (run! :eclector.parse-result))
+
+(defun call-with-input-stream (thunk input &key input-length)
+  (let* ((input (format nil input))
+         (input-length (or input-length (length input))))
+    (with-input-from-string (stream input)
+      (multiple-value-call #'values
+        (funcall thunk stream input-length)
+        (file-position stream)))))
+
+(defun do-read (input eof-error-p eof-value
+                &key
+                (read-function 'eclector.parse-result:read)
+                (client 'simple-result-client))
+  (let ((input (format nil input)))
+    (with-input-from-string (stream input)
+      (values (funcall read-function
+                       (apply 'make-instance (ensure-list client))
+                       stream eof-error-p eof-value)
+              (file-position stream)))))
