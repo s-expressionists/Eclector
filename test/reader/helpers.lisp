7767e6eaab0063db4766cdf03b6ae826a15deeab
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2020-04-03 18:30:28 +0200

    tests: Add helper RELAXED-EQUALP
    And use it in the EXPAND-QUASIQUOTE/SMOKE test.

---
+++ test/reader/helpers.lisp
@@ -0,0 +1,31 @@
+(cl:in-package #:eclector.reader.test)
+
+(defun relaxed-equalp (expected value)
+  "Examples:
+
+  (relaxed-equalp \"foo\" \"FOO\") => nil
+
+  (relaxed-equalp '#:foo '#:foo) => t
+
+  (relaxed-equalp #(1 2) #(1 2)) => t
+
+  (relaxed-equalp '(foo #:bar 1) '(foo #:bar 1)) => t"
+  (labels ((mappablep (thing)
+             (and (typep thing 'sequence)
+                  (or (not (listp thing))
+                      (alexandria:proper-list-p thing))))
+           (rec (expected value)
+             (cond ((stringp expected)
+                    (equal expected value))
+                   ((mappablep expected)
+                    (and (eq (class-of expected) (class-of value))
+                         (= (length expected) (length value))
+                         (every #'relaxed-equalp expected value)))
+                   ((not (symbolp expected))
+                    (equalp expected value))
+                   ((not (symbol-package expected))
+                    (string= (symbol-name expected)
+                             (symbol-name value)))
+                   (t
+                    (eq expected value)))))
+    (rec expected value)))
