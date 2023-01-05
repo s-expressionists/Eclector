ba29987894f2353808a3095cc2c0ea155f6f2d05
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2023-01-05 14:25:39 +0100

    Fixup hash table keys in addition to values
    This change is relevant only when a user has added custom syntax for
    hash table literals and an expression like
    
      #1=#{#1# :something 1 2 3 4}
    
    is read.

--- code/reader/fixup.lisp
+++ code/reader/fixup.lisp
@@ -38,10 +38,33 @@
             do (fixup-place (slot-value object name))))
 
   (defmethod fixup (client (object hash-table) seen-objects mapping)
-    (maphash (lambda (key val)
-               (multiple-value-bind (value found-p)
-                   (gethash val mapping)
-                 (if found-p
-                     (setf (gethash key object) value)
-                     (fixup client val seen-objects mapping))))
-             object)))
+    (let ((key-changes '()))
+      (maphash (lambda (key value)
+                 ;; If KEY has to be replaced, remove the entry for
+                 ;; KEY and VALUE (which is the only entry that may be
+                 ;; removed or changed according to the traversal
+                 ;; rules) and store the information for adding the
+                 ;; final key in KEY-CHANGES (since the traversal
+                 ;; rules forbid adding new entries). Fix up VALUE by
+                 ;; either fixing up the (gethash key object) place or
+                 ;; fixing up the value part of the queued KEY-CHANGES
+                 ;; entry.
+                 (macrolet ((fixup-value (place value)
+                              `(multiple-value-bind (final-value found-p)
+                                   (gethash ,value mapping)
+                                 (if found-p
+                                     (setf ,place final-value)
+                                     (fixup client ,value seen-objects mapping)))))
+                   (multiple-value-bind (final-key found-p)
+                       (gethash key mapping)
+                     (cond (found-p
+                            (remhash key object)
+                            (let ((change (cons final-key value)))
+                              (fixup-value (cdr change) value)
+                              (push change key-changes)))
+                           (t
+                            (fixup client key seen-objects mapping)
+                            (fixup-value (gethash key object) value))))))
+               object)
+      (loop for (final-key . final-value) in key-changes
+            do (setf (gethash final-key object) final-value)))))
