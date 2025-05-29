250b34ec9d2647eb2bdc01b3842a1f50dd249b43
Jan Moringen <jan.moringen@posteo.de>
2025-05-29 20:43:02 +0200

    test: Check parse results in test recover/labeled-objects
    This change makes the test more strict and also uncovered a bug which
    is why one of the existing test cases have to be disabled until the
    bug is fixed.

--- test/parse-result/recover.lisp
+++ test/parse-result/recover.lisp
@@ -3,16 +3,22 @@
 (def-suite* :eclector.parse-result.recover
   :in :eclector.parse-result)
 
-(defun read-with-simple-result-client (stream)
-  (let ((client (make-instance 'simple-result-client)))
-    (raw (eclector.parse-result:read client stream))))
+(defun read-with-list-result-client (stream)
+  (let ((client (make-instance 'list-result-client)))
+    (eclector.parse-result:read client stream)))
 
 (test recover/labeled-objects
   "Test recovering from syntax errors related to labeled objects."
-  (mapc (alexandria:rcurry #'eclector.reader.test::do-recover-test-case
-                           #'read-with-simple-result-client)
-        '(("#1=#1#" (eclector.reader:sharpsign-equals-only-refers-to-self)
-                    nil)
-          ("(#1="   (eclector.reader:end-of-input-after-sharpsign-equals
-                     eclector.reader:unterminated-list)
-                    (nil)))))
+  (mapc (alexandria:rcurry #'eclector.test:do-recover-test-case
+                           #'read-with-list-result-client)
+        '(#+fails ("#1=#1#"
+           (eclector.reader:sharpsign-equals-only-refers-to-self)
+           (:result   nil
+            :children ()
+            :source   (0 . 6)))
+          ("(#1="
+           (eclector.reader:end-of-input-after-sharpsign-equals
+            eclector.reader:unterminated-list)
+           (:result   (nil)
+            :children ((:result nil :children () :source (1 . 4)))
+            :source   (0 . 4))))))
