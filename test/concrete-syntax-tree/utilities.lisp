056a9e6ca3afac81ce059cb39e5b08ffc87dd803
Jan Moringen <jan.moringen@posteo.de>
2025-06-03 00:18:02 +0200

    test: IS-CONSISTENT-WITH-RAW checks identities of certain CST nodes
    For example, a CST, say RESULT, produced by READing the expression
    
      (#1=#P"foo" #1#)
    
    (without explicit representation of labeled object definitions and
    references) definitely has the property
    
      (eq (cst:raw (cst:first result)) (cst:raw (cst:second result)))
    
    but it should also have the property
    
      (eq (cst:first result) (cst:second result))

--- test/concrete-syntax-tree/utilities.lisp
+++ test/concrete-syntax-tree/utilities.lisp
@@ -7,7 +7,8 @@
       `(values (,cst-operator ,cst) (,operator (cst:raw ,cst))))))
 
 (defun is-consistent-with-raw (cst)
-  (let ((seen (make-hash-table :test #'eq)))
+  (let ((seen (make-hash-table :test #'eq))
+        (atom->cst (make-hash-table :test #'eq)))
     (labels ((rec (cst)
                (if (gethash cst seen)
                    (return-from rec)
@@ -17,15 +18,39 @@
                    (multiple-value-bind (cst-atom-p raw-atom-p)
                        (both (atom cst))
                      (is (eq raw-atom-p cst-atom-p))
-                     (unless raw-atom-p
-                       (multiple-value-bind (cst-car raw-car)
-                           (both (first cst))
-                         (multiple-value-bind (cst-cdr raw-cdr)
-                             (both (rest cst))
-                           (is (eq raw-car (cst:raw cst-car)))
-                           (is (eq raw-cdr (cst:raw cst-cdr)))
-                           (rec cst-car)
-                           (rec cst-cdr))))))))
+                     (if raw-atom-p
+                         (let ((raw-atom (cst:raw cst)))
+                           ;; For atoms like pathnames, instances of
+                           ;; structure classes or instances of
+                           ;; standard classes, we expect multiple
+                           ;; `eq' occurrences in the raw expression
+                           ;; to have `eq' corresponding CSTs.
+                           ;;
+                           ;; We may have to refine this later since
+                           ;; something like
+                           ;;
+                           ;;   (is-consistent-with-raw
+                           ;;    (let ((* #P"foo"))
+                           ;;      (eclector.concrete-syntax-tree:read-from-string
+                           ;;       "(#.* #.*)")))
+                           ;;
+                           ;; would currently result in a false positive.
+                           (unless (typep raw-atom '(or number
+                                                        character
+                                                        symbol))
+                             (multiple-value-bind (existing-cst foundp)
+                                 (gethash raw-atom atom->cst)
+                               (if foundp
+                                   (is (eq cst existing-cst))
+                                   (setf (gethash raw-atom atom->cst) cst)))))
+                         (multiple-value-bind (cst-car raw-car)
+                             (both (first cst))
+                           (multiple-value-bind (cst-cdr raw-cdr)
+                               (both (rest cst))
+                             (is (eq raw-car (cst:raw cst-car)))
+                             (is (eq raw-cdr (cst:raw cst-cdr)))
+                             (rec cst-car)
+                             (rec cst-cdr))))))))
       (rec cst))))
 
 (defun valid-cst-parse-result-p (client root-cst)
